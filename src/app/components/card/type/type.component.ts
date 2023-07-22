15ac682022e532672972a2ddfad01386a4382a21
Bill Carioca <billcarioca@hotmail.com>
2023-07-22 10:15:02 -0300

    commit style card and home

--- src/app/components/card/type/type.component.ts
+++ src/app/components/card/type/type.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-type',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type.component.css']
 })
 export class TypeComponent implements OnInit {
+
+  @Input()
+  typesPokemom:string[] = ["Elétrico","Pedra"]
 
   constructor() { }
 
