0468d5e601c723cb188a6fe6d5bedc7b9148547f
Bill Carioca <billcarioca@hotmail.com>
2023-07-22 08:38:00 -0300

    commit create componet and menu style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TitleComponent } from './components/card/title/title.component';
+import { TypeComponent } from './components/card/type/type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    CardComponent,
+    HomeComponent,
+    TitleComponent,
+    TypeComponent
   ],
   imports: [
     BrowserModule,
