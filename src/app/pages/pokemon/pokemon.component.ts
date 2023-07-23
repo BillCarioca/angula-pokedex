114a276f54260c6d39926c8eff59abd71d965f56
Bill Carioca <billcarioca@hotmail.com>
2023-07-23 14:10:38 -0300

    commit page pokemon update

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { pokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -8,25 +10,49 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PokemonComponent implements OnInit {
 
+  pokemon: pokemonData 
   namePokemon:string;
+  typePokemon: string = "normal"
   panelAbout: boolean = true;
   panelBaseStats: boolean = false;
   panelEvolutionChain: boolean = false;
   panelMoves: boolean = false;
 
-  constructor(private activeRoute: ActivatedRoute, private navegador: Router) { 
+  constructor(private service:PokemonService,private activeRoute: ActivatedRoute, private navegador: Router) { 
     this.namePokemon =""
-    
+    this.pokemon ={
+      id :0,
+      name: "",
+      sprites:{front_default:""},
+      types:[]
+    }
     this.activeRoute.params.subscribe(
       res =>{
         this.namePokemon = res['name'],
-        console.log(res)
+        this.ngOnInit()
       } 
     )
     console.log(this.namePokemon)
   }
 
   ngOnInit(): void {
+    this.service.getPokemon(this.namePokemon).subscribe(
+      {
+        next: (res)=> {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+          this.typePokemon = res.types[0].type.name
+          console.log(this.pokemon)
+          console.log(res)
+        },
+        error: (err)=> console.log(err)
+        
+      }
+    )
   }
   voltar(): void{
     this.navegador.navigate(['/'])
